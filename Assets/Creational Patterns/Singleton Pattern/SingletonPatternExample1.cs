b8ff7756e055cfc6aeb0b9b874dae9b583c84de1
dingxiaowei <1213250243@qq.com>
2020-04-11 11:27:20 +0800

    提交

---
+++ Assets/Creational Patterns/Singleton Pattern/SingletonPatternExample1.cs
@@ -0,0 +1,16 @@
+﻿//-------------------------------------------------------------------------------------
+// Title: 单例模式
+// Author: Aladdin
+// Tips: 演示一个最基本的单例模式
+// More: http://dingxiaowei.cn/tags/设计模式/
+//-------------------------------------------------------------------------------------
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingletonPatternExample1 : MonoBehaviour
+{
+    
+}
--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Unity- Design-Patterns
+Unity设计模式
+
 ## 开发环境
 Unity2019.3
+
+## 配套文档教程
+* [前言](http://dingxiaowei.cn/2017/05/10/)
+* [七大设计原则](http://dingxiaowei.cn/2017/05/11/)
+* [UML入门](http://dingxiaowei.cn/2017/05/12/)
+* 创建型设计模式
+	* [单例模式]()
+
+## 更多教程
+http://dingxiaowei.cn
