4c3f1fa8b8063b6f98566eece54f056d3d04c56c
dingxiaowei <1213250243@qq.com>
2020-04-15 22:53:05 +0800

    1

--- Assets/Creational Patterns/Factory Design Pattern/工厂方法模式/FactoryMethodPatternExample.cs
+++ Assets/Creational Patterns/Factory Design Pattern/工厂方法模式/FactoryMethodPatternExample.cs
@@ -9,11 +9,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+                                          \
+/// <summary>
+/// 武器工厂抽象基类
+/// </summary>
+public abstract class WeaponFactoryBase
+{
+    public abstract Weapon CreateWeapon(string name);
+}
+
+/// <summary>
+/// 手枪生产工厂
+/// </summary>
+public class PistolFactory : WeaponFactoryBase
+{
+    public override Weapon CreateWeapon(string name)
+    {
+        return new Pistol(name);
+    }
+}
+
+/// <summary>
+/// 步枪生产工厂
+/// </summary>
+public class RifleFactory : WeaponFactoryBase
+{
+    public override Weapon CreateWeapon(string name)
+    {
+        return new Rifle(name);
+    }
+}
 
 public class FactoryMethodPatternExample : MonoBehaviour
 {
     void Start()
     {
-        
+        //生产一个手枪
+        PistolFactory pistolFactory = new PistolFactory();
+        pistolFactory.CreateWeapon("王八盒子");
+
+        RifleFactory rifleFactory = new RifleFactory();
+        rifleFactory.CreateWeapon("AK47");
     }
 }
