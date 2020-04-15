7b68015e8e7ae4bc4dc8b0735cacc3addc8f2889
dingxiaowei <1213250243@qq.com>
2020-04-15 12:33:53 +0800

    修改

---
+++ Assets/Creational Patterns/Factory Design Pattern/不使用工厂方法/NoneFactoryDesignPatternExample.cs
@@ -0,0 +1,46 @@
+﻿//-------------------------------------------------------------------------------------
+// Title: None工厂模式
+// Author: Aladdin
+// Tips: 演示不实用工厂模式创建各种武器对象
+// More: http://dingxiaowei.cn/tags/设计模式/
+//-------------------------------------------------------------------------------------
+
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Weapon
+{
+    public string Name { get; set; }
+}
+
+//手枪
+public class Pistol : Weapon
+{
+    public Pistol(string name)
+    {
+        this.Name = name;
+        Debug.Log(string.Format("这是一个{0}手枪", name));
+    }
+}
+
+//步枪
+public class Rifle : Weapon
+{
+    public Rifle(string name)
+    {
+        this.Name = name;
+        Debug.Log(string.Format("这是一个{0}步枪", name));
+    }
+}
+
+public class NoneFactoryDesignPatternExample : MonoBehaviour
+{
+    void Start()
+    {
+        Weapon weapon1 = new Pistol("王八盒子");
+        Weapon weapon2 = new Rifle("AK47");
+    }
+}
--- Assets/Creational Patterns/Factory Design Pattern/简单工厂模式/SimpleFactoryPatternExample.cs
+++ Assets/Creational Patterns/Factory Design Pattern/简单工厂模式/SimpleFactoryPatternExample.cs
@@ -11,10 +11,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum WeaponType
+{
+    None = 0,
+    EPistol,
+    ERifle
+}
+
+public class WeaponFactory
+{
+    public Weapon Create(WeaponType type, string name)
+    {
+        Weapon weapon = null;
+        switch (type)
+        {
+            case WeaponType.EPistol:
+                weapon = new Pistol(name);
+                break;
+            case WeaponType.ERifle:
+                weapon = new Rifle(name);
+                break;
+        }
+        return weapon;
+    }
+}
+
 public class SimpleFactoryPatternExample : MonoBehaviour
 {
     void Start()
     {
-        
+        WeaponFactory weaponFactory = new WeaponFactory();
+        Weapon weapon1 = weaponFactory.Create(WeaponType.EPistol, "王八盒子");
+        Weapon weapon2 = weaponFactory.Create(WeaponType.ERifle, "AK47");
     }
 }
