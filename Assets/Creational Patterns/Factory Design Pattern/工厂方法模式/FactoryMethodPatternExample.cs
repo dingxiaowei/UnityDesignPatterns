9bba28561d1f18321dc47f2183f0847d6d9f790b
dingxiaowei <1213250243@qq.com>
2020-04-16 12:58:44 +0800

    抽象工厂

--- Assets/Creational Patterns/Factory Design Pattern/工厂方法模式/FactoryMethodPatternExample.cs
+++ Assets/Creational Patterns/Factory Design Pattern/工厂方法模式/FactoryMethodPatternExample.cs
@@ -9,7 +9,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-                                          \
+                                          
 /// <summary>
 /// 武器工厂抽象基类
 /// </summary>
--- Assets/Creational Patterns/Factory Design Pattern/抽象工厂模式/AbstractFactoryPatternExample.cs
+++ Assets/Creational Patterns/Factory Design Pattern/抽象工厂模式/AbstractFactoryPatternExample.cs
@@ -9,10 +9,80 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// 国家工厂基类
+/// </summary>
+public abstract class CountryWeaponFactoryBase
+{
+    string name;
+    public WeaponFactoryBase RifleFactory;
+    public WeaponFactoryBase PistolFactory;
+    public virtual void InitFactory(string factoryName)
+    {
+        name = factoryName;
+        RifleFactory = new RifleFactory();
+        PistolFactory = new PistolFactory();
+    }
+    public abstract void CreateRifle(string name);
+    public abstract void CreatePistol(string name);
+}
+
+/// <summary>
+/// 中国武器工厂
+/// </summary>
+public class ChineseWeaponFactory : CountryWeaponFactoryBase
+{
+    public override void InitFactory(string factoryName)
+    {
+        base.InitFactory(factoryName);
+        Debug.Log(string.Format("初始化{0},并做一些个性化设置",factoryName));
+    }
+
+    public override void CreatePistol(string name)
+    {
+        base.PistolFactory.CreateWeapon(name);
+    }
+
+    public override void CreateRifle(string name)
+    {
+        base.RifleFactory.CreateWeapon(name);
+    }
+}
+
+/// <summary>
+/// 美国武器工厂
+/// </summary>
+public class AmericanWeaponFactory : CountryWeaponFactoryBase
+{
+    public override void InitFactory(string factoryName)
+    {
+        base.InitFactory(factoryName);
+        Debug.Log(string.Format("初始化{0},并做一些个性化设置", factoryName));
+    }
+
+    public override void CreatePistol(string name)
+    {
+        base.PistolFactory.CreateWeapon(name);
+    }
+
+    public override void CreateRifle(string name)
+    {
+        base.RifleFactory.CreateWeapon(name);
+    }
+}
+
 public class AbstractFactoryPatternExample : MonoBehaviour
 {
     void Start()
     {
-        
+        CountryWeaponFactoryBase chineseWeaponFactory = new ChineseWeaponFactory();
+        chineseWeaponFactory.InitFactory("中国军工厂");
+        chineseWeaponFactory.CreatePistol("92式手枪");
+        chineseWeaponFactory.CreateRifle("95式自动步枪");
+
+        CountryWeaponFactoryBase americanWeaponFactory = new AmericanWeaponFactory();
+        americanWeaponFactory.InitFactory("美国武器工厂");
+        americanWeaponFactory.CreatePistol("MK23手枪");
+        americanWeaponFactory.CreateRifle("M-16步枪");
     }
 }
