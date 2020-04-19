778110e2073d85d3fb466c98b4a1d0a5c7e0562e
dingxiaowei <1213250243@qq.com>
2020-04-19 23:06:28 +0800

    修改

---
+++ Assets/Creational Patterns/Prototype Pattern/PropertyPatternExample2.cs
@@ -0,0 +1,18 @@
+﻿//-------------------------------------------------------------------------------------
+// Title: 原型模式模式
+// Author: Aladdin
+// Tips: 演示游戏开发中小怪的克隆
+// More: http://dingxiaowei.cn/tags/设计模式/
+//-------------------------------------------------------------------------------------
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropertyPatternExample2 : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+}
