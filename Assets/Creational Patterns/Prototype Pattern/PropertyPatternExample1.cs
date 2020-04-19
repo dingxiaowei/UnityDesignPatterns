cb85dd920576ae55a3d9047469c50467e647bea0
dingxiaowei <1213250243@qq.com>
2020-04-19 15:08:48 +0800

    修改

--- Assets/Creational Patterns/Prototype Pattern/PropertyPatternExample1.cs
+++ Assets/Creational Patterns/Prototype Pattern/PropertyPatternExample1.cs
@@ -54,6 +54,43 @@ public class Person : ICloneable
     }
 }
 
+
+//代码优化
+
+[Serializable]
+public class Person1 : BaseClone<Person1>
+{
+    public int Age { get; set; }
+    public string Name { get; set; }
+    public Job Job { get; set; }
+}
+
+//通用深拷贝基类
+[Serializable]
+public class BaseClone<T> : ICloneable where T : new()
+{
+    //浅拷贝
+    public virtual T ShallowClone()
+    {
+        return (T)this.Clone();
+    }
+
+    //深拷贝
+    public virtual T DeepClone()
+    {
+        MemoryStream memoryStream = new MemoryStream();
+        BinaryFormatter formatter = new BinaryFormatter();
+        formatter.Serialize(memoryStream, this);
+        memoryStream.Position = 0;
+        return (T)formatter.Deserialize(memoryStream);
+    }
+
+    public object Clone()
+    {
+        return this.MemberwiseClone();//淺拷贝
+    }
+}
+
 public class PropertyPatternExample1 : MonoBehaviour
 {
     void Start()
@@ -79,6 +116,26 @@ public class PropertyPatternExample1 : MonoBehaviour
         Str = string.Format("修改后：p1.Name={0},p1.Age={1},p1.Job.Id={2},p1.Job.JobName={3}", p1.Name, p1.Age, p1.Job.Id, p1.Job.JobName);
         Debug.Log(Str);
         Str = string.Format("修改后：p2.Name={0},p2.Age={1},p2.Job.Id={2},p2.Job.JobName={3}", p2.Name, p2.Age, p2.Job.Id, p2.Job.JobName);
+        Debug.Log(Str);
+
+        Debug.Log("*******************************************测试通用Clone******************************************************************");
+
+        Person1 newP = new Person1() { Name = "newP", Age = 21, Job = new Job() { JobName = "Coder", Id = 1001 } };
+        Person1 newP2 = newP.DeepClone();
+
+        Str = string.Format("修改前：newP.Name={0},newP.Age={1},newP.Job.Id={2},newP.Job.JobName={3}", newP.Name, newP.Age, newP.Job.Id, newP.Job.JobName);
+        Debug.Log(Str);
+        Str = string.Format("修改前：newP2.Name={0},newP2.Age={1},newP2.Job.Id={2},newP2.Job.JobName={3}", newP2.Name, newP2.Age, newP2.Job.Id, newP2.Job.JobName);
+        Debug.Log(Str);
+
+        newP2.Name = "PM";
+        newP2.Age = 30;
+        newP2.Job.JobName = "Manager";
+        newP2.Job.Id = 1002;
+
+        Str = string.Format("修改前：newP.Name={0},newP.Age={1},newP.Job.Id={2},newP.Job.JobName={3}", newP.Name, newP.Age, newP.Job.Id, newP.Job.JobName);
+        Debug.Log(Str);
+        Str = string.Format("修改后：newP2.Name={0},newP2.Age={1},newP2.Job.Id={2},newP2.Job.JobName={3}", newP2.Name, newP2.Age, newP2.Job.Id, newP2.Job.JobName);
         Debug.Log(Str);
     }
 }
