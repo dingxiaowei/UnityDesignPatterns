6335142ac55c49fa968b3517c875258612b0ea61
dingxiaowei <1213250243@qq.com>
2020-05-08 23:13:58 +0800

    原型模式补充

---
+++ Assets/Creational Patterns/Prototype Pattern/CharacterItem.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using UnityEngine;
+
+[System.Serializable]
+public class Attributes : ICloneable
+{
+    [Tooltip("角色血量")]
+    public int hp;
+    [Tooltip("物理攻击")]
+    public int pAtk;
+    [Tooltip("物理防御")]
+    public int pDef;
+    [Tooltip("魔法攻击")]
+    public int mAtk;
+    [Tooltip("魔法防御")]
+    public int mDef;
+    [Tooltip("行动速度")]
+    public int spd;
+
+    public GameObject model;
+
+    public object Clone()
+    {
+        return this.MemberwiseClone();
+    }
+
+    public Person ShallowClone()
+    {
+        return this.Clone() as Person;
+    }
+
+    public Attributes DeepClone()
+    {
+        Attributes result = new Attributes();
+        result.hp = hp;
+        result.pAtk = pAtk;
+        result.pDef = pDef;
+        result.mAtk = mAtk;
+        result.mDef = mDef;
+        result.spd = spd;
+        return result;
+    }
+
+    public override string ToString()
+    {
+        return string.Format("hp:{0},pAtk:{1} ...", hp, pAtk);
+    }
+}
+
+[CreateAssetMenu(fileName = "CharacterItem", menuName = "(ScriptableObject)CharacterItem")]
+public class CharacterItem : ScriptableObject
+{
+    public string Name;
+    public string Desc;
+    [Tooltip("角色属性")]
+    public Attributes Attributes;
+}
--- Assets/Creational Patterns/Prototype Pattern/PropertyPatternExample2.cs
+++ Assets/Creational Patterns/Prototype Pattern/PropertyPatternExample2.cs
@@ -1,7 +1,7 @@
 ﻿//-------------------------------------------------------------------------------------
 // Title: 原型模式模式
 // Author: Aladdin
-// Tips: 演示游戏开发中小怪的克隆
+// Tips: 演示游戏开发中原型模式的典型应用角色实例化
 // More: http://dingxiaowei.cn/tags/设计模式/
 //-------------------------------------------------------------------------------------
 
@@ -9,10 +9,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public class CharacterEntity
+{
+    public int id;
+    public Attributes objectAttributes;
+}
+
 public class PropertyPatternExample2 : MonoBehaviour
 {
+    public CharacterItem CharDataModel;
     void Start()
     {
+        CharacterEntity char1 = new CharacterEntity() { id = 0, objectAttributes = CharDataModel.Attributes.DeepClone() };
+        CharacterEntity char2 = new CharacterEntity() { id = 1, objectAttributes = CharDataModel.Attributes.DeepClone() };
+        Debug.Log("修改之前两个角色的属性:");
+        Debug.Log("char1 property:" + char1.objectAttributes.ToString());
+        Debug.Log("char2 property:" + char2.objectAttributes.ToString());
 
+        Debug.Log("修改之后两个角色的属性");
+        char1.objectAttributes.hp = 110;
+        Debug.Log("char1 property:" + char1.objectAttributes.ToString());
+        Debug.Log("char2 property:" + char2.objectAttributes.ToString());
     }
 }
